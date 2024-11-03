ec82160476bf0dea7e5a9ce78a7340dbb3f50990
leesache <leesache@student.21-school.ru>
2024-11-03 13:27:22 +0300

    fixed some bug which caused struct overflow with unnecessary

--- src/backend.go
+++ src/backend.go
@@ -41,7 +41,6 @@ type TableContent struct {
 }
 
 type TrainingDay struct {
-	// Day           uint16 // Is this thing needed?
 	ExcerciseName []string
 	Rep           []Excercise
 	CurrentWeight []float32
@@ -53,21 +52,11 @@ type Excercise struct {
 	Max  int
 }
 
-// func fillDay (table TableContent, train []TrainingDay) {
-// 	for i := 0; i < int(table.RowsNum); i++{
-// 		for j := 0; j < int(table.ColsNum); j++{
-
-// 		}
-// 	}
-// }
-
 func fillTrainingDay(table TableContent) []TrainingDay {
-	// Determine the number of training days based on columns
 	numTrainingDays := int(table.ColsNum) / 4
 	fmt.Print(numTrainingDays)
-	train := make([]TrainingDay, numTrainingDays) // Initialize slice with appropriate size
+	train := make([]TrainingDay, numTrainingDays)
 
-	// Use helper functions to fill in each TrainingDay
 	fillExcerciseName(table, train)
 	fillRep(table, train)
 	fillCurrentWeight(table, train)
@@ -76,12 +65,16 @@ func fillTrainingDay(table TableContent) []TrainingDay {
 }
 
 func fillExcerciseName(table TableContent, train []TrainingDay) {
+	rowsPerDay := int(table.RowsNum) / len(train)
+
 	for i := 0; i < len(train); i++ { // iterating through each training day
-		for j := 0; j < int(table.RowsNum); j++ { // iterating through rows
+		startRow := i * rowsPerDay
+		endRow := startRow + rowsPerDay
+
+		for j := startRow; j < endRow; j++ {
 			for k := 0; k < int(table.ColsNum); k += 4 { // iterating every fourth column for exercise names
 				exerciseName := table.TableMatrix[j][k]
 				train[i].ExcerciseName = append(train[i].ExcerciseName, exerciseName)
-				// fmt.Printf("Filling ExcerciseName for TrainingDay %d with [%d][%d]: %v\n", i+1, j, k, exerciseName)
 			}
 		}
 	}
@@ -96,10 +89,8 @@ func fillRep(table TableContent, train []TrainingDay) {
 					fmt.Printf("Error parsing Rep format at [%d][%d]: %v\n", j, k, err)
 					continue
 				}
-				// Create and append a new Excercise struct to the Rep slice
 				exercise := Excercise{Sets: sets, Min: min, Max: max}
 				train[i].Rep = append(train[i].Rep, exercise)
-				// fmt.Printf("Filling Rep for TrainingDay %d with [%d][%d]: %+v\n", i+1, j, k, exercise)
 			}
 		}
 	}
@@ -140,30 +131,25 @@ func parseExerciseFormat(exercise string) (int, int, int, error) {
 func fillCurrentWeight(table TableContent, train []TrainingDay) {
 	for i := 0; i < len(train); i++ {
 		for j := 0; j < int(table.RowsNum); j++ {
-			for k := 1; k < int(table.ColsNum); k += 4 { // Iterating over every fourth column, starting from the second
+			for k := 1; k < int(table.ColsNum); k += 4 {
 				str := strings.ReplaceAll(table.TableMatrix[j][k], ",", ".")
 				weight, err := strconv.ParseFloat(str, 32)
 				if err != nil {
 					fmt.Printf("Error parsing weight at [%d][%d]: %v\n", j, k, err)
 					continue
 				}
-				// Append the parsed weight to the CurrentWeight slice
 				train[i].CurrentWeight = append(train[i].CurrentWeight, float32(weight))
-				// fmt.Printf("Filling CurrentWeight for TrainingDay %d with [%d][%d]: %.2f\n", i+1, j, k, weight)
 			}
 		}
 	}
 }
 
 func main() {
-	// Initialize TableContent
 	var table TableContent
-	table.TableMatrix = make([][]string, 0) // Initialize the TableMatrix as a slice of slices
+	table.TableMatrix = make([][]string, 0)
 
-	// Import data from the Excel file
-	training := excelImportData(table) // Pass the address of table and training to modify them
+	training := excelImportData(table)
 
-	// Print all details of each TrainingDay
 	for i, td := range training {
 		fmt.Printf("Training Day %d:\n", i+1)
 		for j, exerciseName := range td.ExcerciseName {
@@ -192,23 +178,21 @@ func excelImportData(table TableContent) []TrainingDay {
 	}
 	table.RowsNum, table.ColsNum = uint16(len(rows)), uint16(len(cols))
 	for i := 0; i < int(table.RowsNum); i++ {
-		row := make([]string, 0, table.ColsNum) // Create a new slice for the current row
+		row := make([]string, 0, table.ColsNum)
 		for j := 0; j < int(table.ColsNum); j++ {
-			cellValue, err := f.GetCellValue(sheetName, colIndex(j+1)+strconv.Itoa(i+1)) // Use i+1 for 1-based row index
+			cellValue, err := f.GetCellValue(sheetName, colIndex(j+1)+strconv.Itoa(i+1))
 			if err != nil {
 				log.Printf("Error getting cell value at row %d, column %d: %v", i, j, err)
 				continue
 			}
-			row = append(row, cellValue) // Append the cell value to the current row
+			row = append(row, cellValue)
 		}
-		table.TableMatrix = append(table.TableMatrix, row) // Append the current row to TableMatrix
+		table.TableMatrix = append(table.TableMatrix, row)
 	}
 
 	return fillTrainingDay(table)
 }
 
 func colIndex(num int) string {
-	// Make AA BB ETC
-
 	return string(rune('A' + (num - 1)))
 }
